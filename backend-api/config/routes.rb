edc6f0e0e9f7b628a6de55f65fcddee56b720cc1
biagioo <mendolia2328@gmail.com>
2020-06-29 13:29:39 -0400

    created resources for score, difficulty and word

--- backend-api/config/routes.rb
+++ backend-api/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :words
+  resources :difficulties
+  resources :scores
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
