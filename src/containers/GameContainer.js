51b7d0b659c3c45b3bedd62a526c6a37e976410d
biagioo <mendolia2328@gmail.com>
2020-07-30 10:04:15 -0400

    added loading to gameContainer

--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -1,29 +1,33 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import {fetchDifficulties} from '../actions/gameActions'
-import GameDifficulties from '../components/game/GameDifficulties'
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchDifficulties} from '../actions/gameActions';
+import GameDifficulties from '../components/game/GameDifficulties';
 
-class GameContainer extends Component{
-    
-    componentDidMount(){
-        this.props.fetchDifficulties()
-    }
-
-    render(){
-        return (
-            <div>
-                <h1>The Typing Game</h1>
-                <GameDifficulties difficulties={this.props.difficulties}/>
-            </div>
-        )
-    }
-
-}
-const mapStateToProps = state => {
-    return {
-      difficulties: state.difficulties.difficulties,
-    }
+class GameContainer extends Component {
+  state = {
+    loading: true,
+  };
+  componentDidMount() {
+    this.props.fetchDifficulties().then(this.setState({loading: false}));
   }
 
-export default connect(mapStateToProps,{fetchDifficulties})(GameContainer)
+  render() {
+    return (
+      <div>
+        <h1>The Typing Game</h1>
+        {this.state.loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <GameDifficulties difficulties={this.props.difficulties} />
+        )}
+      </div>
+    );
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    difficulties: state.difficulties.difficulties,
+  };
+};
 
+export default connect(mapStateToProps, {fetchDifficulties})(GameContainer);
