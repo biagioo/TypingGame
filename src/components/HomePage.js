834d2a50f9c584ef148fbc2d65ddd169b2709b76
biagioo <mendolia2328@gmail.com>
2020-08-29 11:14:34 -0400

    Update instructions on home screen

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,22 +1,30 @@
-import React from 'react'
-import typingGif from '../gif/typing.gif'
+import React from 'react';
+import typingGif from '../gif/typing.gif';
 
-const HomePage = () =>{
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to The Typing Game!</h1>
+      <h3>Instructions:</h3>
 
-    return(
-        <div>
-            <h1>Welcome to The Typing Game!</h1>
-            <h3>Instructions:</h3>
-           
-            <p>Select which difficulty you'd like to play on. </p>
-            <p>Easy is 4 letter words, Medium is 5-6 letter words, Hard is 7 letter words. </p>
-            <p>After the game is over, you'll be asked for a display name for your score to be seen on the score board. </p>
-            <p>The score board only shows the top 3 scores for each difficulty.</p>
-            <p>Enjoy!</p>
-            <img style={{border: '5px solid white',}}src={typingGif} alt="GIf Loading..." />
+      <p>Select which difficulty you'd like to play on. </p>
+      <p>
+        Easy is 4 letter words, Medium is 5-6 letter words, Hard is 7 letter
+        words.
+      </p>
+      <p>
+        After the game is over, you'll be asked for a display name for your
+        score to be seen on the score board. Search for a name and filter
+        through difficulties with the drop down menu.
+      </p>
+      <p>Enjoy!</p>
+      <img
+        style={{border: '5px solid white'}}
+        src={typingGif}
+        alt="GIf Loading..."
+      />
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default HomePage
+export default HomePage;
