a4d4ee73167a994552d67c2acea2496fe7649813
biagioo <mendolia2328@gmail.com>
2020-09-16 10:10:40 -0400

    added opacity animation to HomePage component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import GameContainer from './containers/GameContainer'
-import ScoresContainer from './containers/ScoresContainer'
-import HomePage from './components/HomePage'
+import GameContainer from './containers/GameContainer';
+import ScoresContainer from './containers/ScoresContainer';
+import HomePage from './components/HomePage';
 import './App.css';
-import NavBar from './components/NavBar'
-import { BrowserRouter as Router, Route} from "react-router-dom"
+import NavBar from './components/NavBar';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <NavBar />
-        <Route exact path="/game" render={()=> <GameContainer />} />
+        <Route exact path="/game" render={() => <GameContainer />} />
         <Route exact path="/scores" render={() => <ScoresContainer />} />
-        <Route exact path="/" render={() => <HomePage />} />     
+        <Route exact path="/" render={() => <HomePage />} />
       </div>
     </Router>
   );
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,29 +1,36 @@
 import React from 'react';
+import {Spring} from 'react-spring/renderprops';
 import typingGif from '../gif/typing.gif';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to The Typing Game!</h1>
-      <h3>Instructions:</h3>
+    <Spring from={{opacity: -5}} to={{opacity: 1}}>
+      {(props) => (
+        <div style={props}>
+          <div>
+            <h1>Welcome to The Typing Game!</h1>
+            <h3>Instructions:</h3>
 
-      <p>Select which difficulty you'd like to play on. </p>
-      <p>
-        Easy is 4 letter words, Medium is 5-6 letter words, Hard is 7 letter
-        words.
-      </p>
-      <p>
-        After the game is over, you'll be asked for a display name for your
-        score to be seen on the score board. Search for a name and filter
-        through difficulties with the drop down menu.
-      </p>
-      <p>Enjoy!</p>
-      <img
-        style={{border: '5px solid white'}}
-        src={typingGif}
-        alt="GIf Loading..."
-      />
-    </div>
+            <p>Select which difficulty you'd like to play on. </p>
+            <p>
+              Easy is 4 letter words, Medium is 5-6 letter words, Hard is 7
+              letter words.
+            </p>
+            <p>
+              After the game is over, you'll be asked for a display name for
+              your score to be seen on the score board. Search for a name and
+              filter through difficulties with the drop down menu.
+            </p>
+            <p>Enjoy!</p>
+            <img
+              style={{border: '5px solid white'}}
+              src={typingGif}
+              alt="GIf Loading..."
+            />
+          </div>
+        </div>
+      )}
+    </Spring>
   );
 };
 
