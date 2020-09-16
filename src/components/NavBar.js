0d990498ffd832016cb4d9497e294a0dff6343a6
biagioo <mendolia2328@gmail.com>
2020-09-16 10:13:45 -0400

    added drop down animation to NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
+import {Spring} from 'react-spring/renderprops';
 
 const link = {
   width: '100px',
@@ -7,32 +8,35 @@ const link = {
   margin: '6px 6px 6px',
   background: 'lightseagreen',
   textDecoration: 'none',
-  color: 'white'
-}
+  color: 'white',
+};
 
 const NavBar = () => {
   return (
-    <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
-      <NavLink 
-        style={link} 
-        to="/"
-      >
-        Home
-      </NavLink>
-      <NavLink 
-        style={link} 
-        to="/game"
-      >
-        Game
-      </NavLink>
-      <NavLink 
-        style={link} 
-        to="/scores"
-      >
-        Scores
-      </NavLink>
-    </div>
+    <Spring from={{marginTop: -500}} to={{marginTop: 0}}>
+      {(props) => (
+        <div style={props}>
+          <div
+            style={{
+              borderBottom: '2px solid black',
+              paddingBottom: '10px',
+              marginBottom: '12px',
+            }}
+          >
+            <NavLink style={link} to="/">
+              Home
+            </NavLink>
+            <NavLink style={link} to="/game">
+              Game
+            </NavLink>
+            <NavLink style={link} to="/scores">
+              Scores
+            </NavLink>
+          </div>
+        </div>
+      )}
+    </Spring>
   );
-}
+};
 
 export default NavBar;
